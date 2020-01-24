4d5a3a9b529e7a5ba106c99869aa95a18e6ca25e
dyinglev <dyinglev@yandex.ru>
2020-01-24 20:58:31 +0500

    Enter to app V3

--- libs/EnterForm.py
+++ libs/EnterForm.py
@@ -1,4 +1,5 @@
-from tkinter import *
+from tkinter import Tk, Label, Button
+from tkinter import ttk
 from libs.User import User
 from libs.MainWindow import MainWindow
 
@@ -13,24 +14,27 @@ class EnterForm:
         self.sign_in_form.resizable(False, False)
         width = self.sign_in_form.winfo_screenwidth()
         height = self.sign_in_form.winfo_screenheight()
-        self.sign_in_form.geometry(f'300x150+{width // 2 - 150}+{height // 2 - 85}')  # Центрироване окна
+        # Центрироване окна
+        self.sign_in_form.geometry(f'300x180+{width // 2 - 150}+{height // 2 - 100}')
 
         self.user = User()
 
+        # Объявление виджетов
         self.label_enter = Label(self.sign_in_form, text='Войдите в Yogify', bg='#1e90ff', fg='#fff', font=15, width=34, pady=10)
-
         self.label_login = Label(self.sign_in_form, text='Логин')
-        self.login = Entry(self.sign_in_form)
+        self.login = ttk.Entry(self.sign_in_form, width=30)
+        self.login.focus_set()
         self.label_password = Label(self.sign_in_form, text='Пароль')
-        self.password = Entry(self.sign_in_form)
-        self.btn_sign_in = Button(self.sign_in_form, text='Enter', command=self.check_userdata)
+        self.password = ttk.Entry(self.sign_in_form, width=30)
+        self.btn_sign_in = ttk.Button(self.sign_in_form, text='Войти', command=self.check_userdata, width='14')
 
+        # Расстановка виджетов
         self.label_enter.place(relx=0.5, anchor='n')
-        # self.label_login.place(row=1, column=0)
-        # self.login.place(row=1, column=1)
-        # self.label_password.place(row=2, column=0)
-        # self.password.place(row=2, column=1)
-        # self.btn_sign_in.place(row=3, column=0, columnspan=2)
+        self.label_login.place(x=75, y=70, anchor='e')
+        self.login.place(x=80, y=70, anchor='w')
+        self.label_password.place(x=75, y=100, anchor='e')
+        self.password.place(x=80, y=100, anchor='w')
+        self.btn_sign_in.place(relx=0.5, y=130, anchor='n')
 
         self.sign_in_form.mainloop()
 
